de8dea00b10fa1e2a27a7e6d38c47dcb0e742d4d
Mauro Dalla Zuanna <maurodallazuanna@hotmail.com>
2018-07-14 16:53:38 +0200

    Configurazione common.logging e log4net. Scrittura log eventi windows service.

--- Zu1779.AGE.WindowsService/MainService.cs
+++ Zu1779.AGE.WindowsService/MainService.cs
@@ -2,8 +2,13 @@
 {
     using System.ServiceProcess;
 
+    using Common.Logging;
+    using Newtonsoft.Json;
+
     partial class MainService : ServiceBase
     {
+        private static readonly ILog log = LogManager.GetLogger(nameof(MainService));
+
         public MainService()
         {
             InitializeComponent();
@@ -11,43 +16,43 @@
 
         protected override void OnStart(string[] args)
         {
-            
+            log.Info($"{nameof(MainService)}.{nameof(OnStart)} - {nameof(args)}={JsonConvert.SerializeObject(args)}");
         }
 
         protected override void OnPause()
         {
-            
+            log.Info($"{nameof(MainService)}.{nameof(OnPause)}");
         }
 
         protected override void OnContinue()
         {
-            
+            log.Info($"{nameof(MainService)}.{nameof(OnContinue)}");
         }
 
         protected override void OnCustomCommand(int command)
         {
-            
+            log.Info($"{nameof(MainService)}.{nameof(OnCustomCommand)} - {nameof(command)}={command}");
         }
 
         protected override void OnStop()
         {
-
+            log.Info($"{nameof(MainService)}.{nameof(OnStop)}");
         }
 
         protected override void OnSessionChange(SessionChangeDescription changeDescription)
         {
-            
+            log.Info($"{nameof(MainService)}.{nameof(OnSessionChange)} - {nameof(changeDescription)}={JsonConvert.SerializeObject(changeDescription)}");
         }
 
         protected override bool OnPowerEvent(PowerBroadcastStatus powerStatus)
         {
-
+            log.Info($"{nameof(MainService)}.{nameof(OnPowerEvent)} - {nameof(powerStatus)}={powerStatus}");
             return true;
         }
 
         protected override void OnShutdown()
         {
-            
+            log.Info($"{nameof(MainService)}.{nameof(OnShutdown)}");
         }
     }
 }
