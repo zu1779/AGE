90b4e5085f11a2b591feeefe0e5cbf62d5dcbf28
Mauro Dalla Zuanna <maurodallazuanna@hotmail.com>
2018-08-04 19:18:35 +0200

    Slow working ...

--- Zu1779.AGE.Environment.TestEnvironment/EnvCore/Engine.cs
+++ Zu1779.AGE.Environment.TestEnvironment/EnvCore/Engine.cs
@@ -1,7 +1,32 @@
 ﻿namespace Zu1779.AGE.Env.TestEnv.EnvCore
 {
+    using System;
+
     internal class Engine
     {
-        
+        public Engine()
+        {
+
+        }
+    }
+
+    internal class Location
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public GeoLocation GeoLocation { get; set; }
+    }
+
+    internal class Person
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public GeoLocation GeoLocation { get; set; }
+    }
+
+    internal class GeoLocation
+    {
+        public decimal Lat { get; set; }
+        public decimal Lng { get; set; }
     }
 }
